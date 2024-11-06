27bc3094220e7a17a78e92ad272aa80a856696e7
Ayse I. Kolabas <akolab01@comp117-02.eecs.tufts.edu>
2024-11-05 19:39:18 -0500

    added functions for if else elif formatting

--- simplefunction.proxy.cpp
+++ simplefunction.proxy.cpp
@@ -8,14 +8,38 @@
 using namespace C150NETWORK;
 
 void func1() {
+    int fNameLen = strlen("func1");
+    RPCPROXYSOCKET->write((const char *) &fNameLen, sizeof(fNameLen));
+    RPCPROXYSOCKET->write("func1", strlen("func1"));
 
+    char readBuffer[4];
+    RPCPROXYSOCKET->read(readBuffer, 4);
+    if (strncmp(readBuffer, "DONE", 4) != 0) {
+        throw C150Exception("error");
+    }
 }
 
 void func2() {
+    int fNameLen = strlen("func2");
+    RPCPROXYSOCKET->write((const char *) &fNameLen, sizeof(fNameLen));
+    RPCPROXYSOCKET->write("func2", strlen("func2"));
 
+    char readBuffer[4];
+    RPCPROXYSOCKET->read(readBuffer, 4);
+    if (strncmp(readBuffer, "DONE", 4) != 0) {
+        throw C150Exception("error");
+    }
 }
 
 void func3() {
+    int fNameLen = strlen("func3");
+    RPCPROXYSOCKET->write((const char *) &fNameLen, sizeof(fNameLen));
+    RPCPROXYSOCKET->write("func3", strlen("func3"));
 
+    char readBuffer[4];
+    RPCPROXYSOCKET->read(readBuffer, 4);
+    if (strncmp(readBuffer, "DONE", 4) != 0) {
+        throw C150Exception("error");
+    }
 }
 
--- simplefunction.stub.cpp
+++ simplefunction.stub.cpp
@@ -8,14 +8,47 @@
 using namespace C150NETWORK;
 
 void __func1() {
+    char doneBuffer[5] = "DONE";
 
+    func1();
+
+    RPCSTUBSOCKET->write(doneBuffer, strlen(doneBuffer));
 }
 
 void __func2() {
+    char doneBuffer[5] = "DONE";
 
+    func2();
+
+    RPCSTUBSOCKET->write(doneBuffer, strlen(doneBuffer));
 }
 
 void __func3() {
+    char doneBuffer[5] = "DONE";
 
+    func3();
+
+    RPCSTUBSOCKET->write(doneBuffer, strlen(doneBuffer));
 }
 
+void dispatchFunction() {
+    int fNameLen;
+    RPCSTUBSOCKET->read((char *) &fNameLen, sizeof(fNameLen));
+
+    char functionNameBuffer[fNameLen + 1];
+
+    RPCSTUBSOCKET->read(functionNameBuffer, fNameLen);
+    functionNameBuffer[fNameLen] = '\0';
+
+    if (!RPCSTUBSOCKET-> eof()) {
+        if (strcmp(functionNameBuffer, "func3") == 0) {
+            __func3();
+        } else if (strcmp(functionNameBuffer, "func2") == 0) {
+            __func2();
+        } else if (strcmp(functionNameBuffer, "func1") == 0) {
+            __func1();
+        } else {
+            throw C150Exception("received above function name\n");
+        }
+    }
+}
