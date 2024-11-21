bc3f41aa42e1bbae8989a5fcd75fc56b1c157636
Ayse I. Kolabas <akolab01@comp117-01.eecs.tufts.edu>
2024-11-21 16:56:11 -0500

    more crazy tests - i think we're good

--- idils-crazy-tests.cpp
+++ idils-crazy-tests.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 #include "idils-crazy-tests.idl"
 
@@ -63,4 +67,352 @@ void printPersonsOfPersons(Person people[5][4]) {
                  << ", Age: " << people[i][j].age << endl;
         }
     }
+}
+
+int returnTwo() {
+    return 2;
+}
+
+int returnInputInt(int input) {
+    return input;
+}
+
+int returnInputFloat(float input) {
+    return static_cast<int>(input); 
+}
+
+int returnInputString(string input) {
+    return input.length(); 
+}
+
+int returnAgeOfPerson(Person idil) {
+    return idil.age;
+}
+
+int returnAgeOfRelativeOfPerson(Relative idils_sister) {
+    return idils_sister.age;
+}
+
+int returnAverageAgeOfFamily(FamilyOfThree fot) {
+    int totalAge = 0;
+    for (int i = 0; i < 3; i++) {
+        totalAge += fot.family_members[i].age;
+    }
+    return totalAge / 3; 
+}
+
+int returnAverageAgeOfPersons(Person persons[5]) {
+    int totalAge = 0;
+    for (int i = 0; i < 5; i++) {
+        totalAge += persons[i].age;
+    }
+    return totalAge / 5; 
+}
+
+int returnAverageAgeOfFamilies(FamilyOfThree fot[5]) {
+    int totalAge = 0, totalPeople = 0;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            totalAge += fot[i].family_members[j].age;
+            totalPeople++;
+        }
+    }
+    return totalAge / totalPeople; 
+}
+
+int returnAverageAgeOfPeople(Person people[5][4]) {
+    int totalAge = 0, totalPeople = 0;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 4; j++) {
+            totalAge += people[i][j].age;
+            totalPeople++;
+        }
+    }
+    return totalAge / totalPeople; 
+}
+
+float fReturnTwo() {
+    return 2.5;
+}
+
+float fReturnInputInt(int input) {
+    return static_cast<float>(input) + 0.5;
+}
+
+float fReturnInputFloat(float input) {
+    return input; 
+}
+
+float fReturnInputString(string input) {
+    return static_cast<float>(input.length()); 
+}
+
+float fReturnAgeOfPerson(Person idil) {
+    return static_cast<float>(idil.age);
+}
+
+float fReturnAgeOfRelativeOfPerson(Relative idils_sister) {
+    return static_cast<float>(idils_sister.age);
+}
+
+float fReturnAverageAgeOfFamily(FamilyOfThree fot) {
+    float totalAge = 0;
+    for (int i = 0; i < 3; i++) {
+        totalAge += fot.family_members[i].age;
+    }
+    return totalAge / 3.0; 
+}
+
+float fReturnAverageAgeOfPersons(Person persons[5]) {
+    float totalAge = 0;
+    for (int i = 0; i < 5; i++) {
+        totalAge += persons[i].age;
+    }
+    return totalAge / 5.0; 
+}
+
+float fReturnAverageAgeOfFamilies(FamilyOfThree fot[5]) {
+    float totalAge = 0, totalPeople = 0;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            totalAge += fot[i].family_members[j].age;
+            totalPeople++;
+        }
+    }
+    return totalAge / totalPeople; 
+}
+
+float fReturnAverageAgeOfPeople(Person people[5][4]) {
+    float totalAge = 0, totalPeople = 0;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 4; j++) {
+            totalAge += people[i][j].age;
+            totalPeople++;
+        }
+    }
+    return totalAge / totalPeople; 
+}
+
+
+string intToString(int value) {
+    ostringstream oss;
+    oss << value;
+    return oss.str();
+}
+
+
+string sReturnTwo() {
+    return "2";
+}
+
+string sReturnInputInt(int input) {
+    return intToString(input);
+}
+
+string sReturnInputFloat(float input) {
+    ostringstream oss;
+    oss << input; 
+    return oss.str();
+}
+
+string sReturnInputString(string input) {
+    return "Received string: " + input;
+}
+
+string sReturnAgeOfPerson(Person idil) {
+    return "Age of Person " + idil.name + ": " + intToString(idil.age);
+}
+
+string sReturnAgeOfRelativeOfPerson(Relative idils_sister) {
+    return "Age of Relative " + idils_sister.name + ": " + intToString(idils_sister.age);
+}
+
+string sReturnAverageAgeOfFamily(FamilyOfThree fot) {
+    int totalAge = 0;
+    for (int i = 0; i < 3; i++) {
+        totalAge += fot.family_members[i].age;
+    }
+    int averageAge = totalAge / 3;
+    return "Average age of family " + fot.lastname + ": " + intToString(averageAge);
+}
+
+string sReturnAverageAgeOfPersons(Person persons[5]) {
+    int totalAge = 0;
+    for (int i = 0; i < 5; i++) {
+        totalAge += persons[i].age;
+    }
+    int averageAge = totalAge / 5;
+    return "Average age of persons: " + intToString(averageAge);
+}
+
+string sReturnAverageAgeOfFamilies(FamilyOfThree fot[5]) {
+    int totalAge = 0, totalPeople = 0;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            totalAge += fot[i].family_members[j].age;
+            totalPeople++;
+        }
+    }
+    int averageAge = totalAge / totalPeople;
+    return "Average age of families: " + intToString(averageAge);
+}
+
+string sReturnAverageAgeOfPeople(Person people[5][4]) {
+    int totalAge = 0, totalPeople = 0;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 4; j++) {
+            totalAge += people[i][j].age;
+            totalPeople++;
+        }
+    }
+    int averageAge = totalAge / totalPeople;
+    return "Average age of people: " + intToString(averageAge);
+}
+
+string randomName() {
+    const string names[] = {"Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace"};
+    int index = rand() % (sizeof(names) / sizeof(names[0]));
+    return names[index];
+}
+
+
+Person makeRandomPerson() {
+    srand(time(0));
+    Person randomPerson = {randomName(), rand() % 100}; 
+    return randomPerson;
+}
+
+Person makePersonAgeYearsOld(int age) {
+    return {"Unnamed", age};
+}
+
+Person makePersonAgeAndAHalfYearsOld(float age) {
+    return {"Unnamed", static_cast<int>(age)};
+}
+
+Person makePersonWithName(string name) {
+    return {name, 0};
+}
+
+Person makeCopyOfPerson(Person p) {
+    return p;
+}
+
+Person returnRelativeAYearOlder(Relative idils_sister) {
+    Person olderRelative = {idils_sister.name, idils_sister.age + 1};
+    return olderRelative;
+}
+
+Person returnOldestMember(FamilyOfThree fot) {
+    Person oldest = fot.family_members[0];
+    for (int i = 1; i < 3; i++) {
+        if (fot.family_members[i].age > oldest.age) {
+            oldest = fot.family_members[i];
+        }
+    }
+    return oldest;
+}
+
+Person returnYoungestMember(Person persons[5]) {
+    Person youngest = persons[0];
+    for (int i = 1; i < 5; i++) {
+        if (persons[i].age < youngest.age) {
+            youngest = persons[i];
+        }
+    }
+    return youngest;
+}
+
+Person returnOldestAverageFamily(FamilyOfThree fot[5]) {
+    FamilyOfThree oldestFamily = fot[0];
+    int maxAverageAge = 0;
+
+    for (int i = 0; i < 5; i++) {
+        int totalAge = 0;
+        for (int j = 0; j < 3; j++) {
+            totalAge += fot[i].family_members[j].age;
+        }
+        int averageAge = totalAge / 3;
+        if (averageAge > maxAverageAge) {
+            maxAverageAge = averageAge;
+            oldestFamily = fot[i];
+        }
+    }
+
+    return returnOldestMember(oldestFamily);
+}
+
+Person returnYoungestAveragePeople(Person people[5][4]) {
+    Person youngestPerson = people[0][0];
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (people[i][j].age < youngestPerson.age) {
+                youngestPerson = people[i][j];
+            }
+        }
+    }
+
+    return youngestPerson;
+}
+
+Person createPerson(const string &name, int age) {
+    return {name, age};
+}
+
+FamilyOfThree createFamily(const string &lastname, Person p1, Person p2, Person p3) {
+    FamilyOfThree family;
+    family.lastname = lastname;
+    family.family_members[0] = p1;
+    family.family_members[1] = p2;
+    family.family_members[2] = p3;
+    return family;
+}
+
+FamilyOfThree makeFamilyOfThree() {
+    srand(time(0));
+    return createFamily("Randomson", 
+                        createPerson("Alice", rand() % 100),
+                        createPerson("Bob", rand() % 100),
+                        createPerson("Charlie", rand() % 100));
+}
+
+FamilyOfThree makeFamilyWithAges(int age1, int age2, int age3) {
+    return createFamily("AgesFamily",
+                        createPerson("Person1", age1),
+                        createPerson("Person2", age2),
+                        createPerson("Person3", age3));
+}
+
+FamilyOfThree makeFamilyWithName(string lastname) {
+    return createFamily(lastname,
+                        createPerson("Parent1", 40),
+                        createPerson("Parent2", 38),
+                        createPerson("Child", 12));
+}
+
+FamilyOfThree makeFamilyWithPersons(Person p1, Person p2, Person p3) {
+    return createFamily("CustomFamily", p1, p2, p3);
+}
+
+FamilyOfThree makeFamilyWithPersonArray(Person fofthree[3]) {
+    return createFamily("ArrayFamily", fofthree[0], fofthree[1], fofthree[2]);
+}
+
+FamilyOfThree returnOldestFamily(FamilyOfThree families[5]) {
+    FamilyOfThree oldestFamily = families[0];
+    int maxTotalAge = 0;
+
+    for (int i = 0; i < 5; i++) {
+        int totalAge = families[i].family_members[0].age +
+                       families[i].family_members[1].age +
+                       families[i].family_members[2].age;
+
+        if (totalAge > maxTotalAge) {
+            maxTotalAge = totalAge;
+            oldestFamily = families[i];
+        }
+    }
+
+    return oldestFamily;
 }
--- idils-crazy-testsclient.cpp
+++ idils-crazy-testsclient.cpp
@@ -4,50 +4,36 @@
 using namespace std;
 #include "idils-crazy-tests.idl" // Replace with your actual .idl filename
 
+void displayPerson(Person p) {
+    cout << "Name: " << p.name << ", Age: " << p.age << endl;
+}
+
+void displayFamily(FamilyOfThree family) {
+    cout << "Family Lastname: " << family.lastname << endl;
+    cout << "Family Members:" << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << "  - ";
+        displayPerson(family.family_members[i]);
+    }
+    cout << endl;
+}
+
+FamilyOfThree createFamily(const string &lastname, Person p1, Person p2, Person p3) {
+    FamilyOfThree family;
+    family.lastname = lastname;
+    family.family_members[0] = p1;
+    family.family_members[1] = p2;
+    family.family_members[2] = p3;
+    return family;
+}
+
 int main(int argc, char *argv[]) {
     rpcproxyinitialize(argv[1]);
 
-    // Test each function
-
-    // 1. Test printNone
-    printNone();
-
-    // 2. Test printOne
-    printOne(42);
-
-    // 3. Test printTwo
-    printTwo(3.14);
-
-    // 4. Test printThis
-    printThis("Hello, distributed world!");
-
-    // 5. Test printPerson
     Person idil = {"Idil Kolabas", 22};
-    printPerson(idil);
-
-    // 6. Test printRelative
     Relative idils_sister = {"Sister Kolabas", 25, idil};
-    printRelative(idils_sister);
-
-    // 7. Test printFamily
     FamilyOfThree fot = {"Kolabas", {{"Idil", 22}, {"Sister", 25}, {"Parent", 50}}};
-    printFamily(fot);
-
-    // 8. Test printPersons
     Person persons[5] = {{"Alice", 30}, {"Bob", 35}, {"Charlie", 40}, {"David", 45}, {"Eve", 50}};
-    printPersons(persons);
-
-    // 9. Test printFamilies
-    FamilyOfThree families[5] = {
-        {"Smith", {{"John", 40}, {"Jane", 38}, {"Jack", 15}}},
-        {"Doe", {{"Joe", 42}, {"Jill", 39}, {"Jenny", 16}}},
-        {"Brown", {{"Jim", 45}, {"Joan", 43}, {"Josh", 18}}},
-        {"Taylor", {{"Tim", 50}, {"Tina", 48}, {"Tom", 20}}},
-        {"White", {{"Will", 55}, {"Wendy", 52}, {"Walter", 25}}}
-    };
-    printFamilies(families);
-
-    // 10. Test printPersonsOfPersons
     Person people[5][4] = {
         {{"Alice", 30}, {"Bob", 31}, {"Charlie", 32}, {"David", 33}},
         {{"Eve", 34}, {"Frank", 35}, {"Grace", 36}, {"Hank", 37}},
@@ -55,7 +41,145 @@ int main(int argc, char *argv[]) {
         {{"Mia", 42}, {"Nancy", 43}, {"Oscar", 44}, {"Paul", 45}},
         {{"Quinn", 46}, {"Rachel", 47}, {"Steve", 48}, {"Tina", 49}}
     };
+    FamilyOfThree families[5] = {
+        {"Smith", {{"John", 40}, {"Jane", 38}, {"Jack", 15}}},
+        {"Doe", {{"Joe", 42}, {"Jill", 39}, {"Jenny", 16}}},
+        {"Brown", {{"Jim", 45}, {"Joan", 43}, {"Josh", 18}}},
+        {"Taylor", {{"Tim", 50}, {"Tina", 48}, {"Tom", 20}}},
+        {"White", {{"Will", 55}, {"Wendy", 52}, {"Walter", 25}}}
+    };
+
+
+    // void
+    printNone();
+    printOne(42);
+    printTwo(3.14);
+    printThis("Hello, distributed world!");
+    printPerson(idil);
+    printRelative(idils_sister);
+    printFamily(fot);
+    printPersons(persons);
+    printFamilies(families);
     printPersonsOfPersons(people);
+
+    // int returning tests
+    cout << "returnTwo: " << returnTwo() << endl;
+    cout << "returnInputInt(42): " << returnInputInt(42) << endl;
+    cout << "returnInputFloat(3.14): " << returnInputFloat(3.14) << endl;
+    cout << "returnInputStringLength(\"Hello\"): " << returnInputString("Hello") << endl;
+    cout << "returnAgeOfPerson(Idil): " << returnAgeOfPerson(idil) << endl;
+    cout << "returnAgeOfRelativeOfPerson(Idil's Sister): " 
+         << returnAgeOfRelativeOfPerson(idils_sister) << endl;
+    cout << "returnAverageAgeOfFamily(Kolabas): " 
+         << returnAverageAgeOfFamily(fot) << endl;
+    cout << "returnAverageAgeOfPersons: " << returnAverageAgeOfPersons(persons) << endl;
+    cout << "returnAverageAgeOfFamilies: " 
+         << returnAverageAgeOfFamilies(families) << endl;
+    cout << "returnAverageAgeOfPeople: " << returnAverageAgeOfPeople(people) << endl;
+
+    // float returning tests
+    cout << "returnTwo: " << fReturnTwo() << endl;
+    cout << "returnInputInt(42): " << fReturnInputInt(42) << endl;
+    cout << "returnInputFloat(3.14): " << fReturnInputFloat(3.14) << endl;
+    cout << "returnInputStringLength(\"Hello\"): " << fReturnInputString("Hello") << endl;
+    cout << "returnAgeOfPerson(Idil): " << fReturnAgeOfPerson(idil) << endl;
+    cout << "returnAgeOfRelativeOfPerson(Idil's Sister): " 
+         << fReturnAgeOfRelativeOfPerson(idils_sister) << endl;
+    cout << "returnAverageAgeOfFamily(Kolabas): " 
+         << fReturnAverageAgeOfFamily(fot) << endl;
+    cout << "returnAverageAgeOfPersons: " << fReturnAverageAgeOfPersons(persons) << endl;
+    cout << "returnAverageAgeOfFamilies: " 
+         << fReturnAverageAgeOfFamilies(families) << endl;
+    cout << "returnAverageAgeOfPeople: " << fReturnAverageAgeOfPeople(people) << endl;
+
+    // string returning tests
+    cout << sReturnTwo() << endl;
+    cout << sReturnInputInt(42) << endl;
+    cout << sReturnInputFloat(3.14) << endl;
+    cout << sReturnInputString("Hello, distributed world!") << endl;
+    cout << sReturnAgeOfPerson(idil) << endl;
+    cout << sReturnAgeOfRelativeOfPerson(idils_sister) << endl;
+    cout << sReturnAverageAgeOfFamily(fot) << endl;
+    cout << sReturnAverageAgeOfPersons(persons) << endl;
+    cout << sReturnAverageAgeOfFamilies(families) << endl;
+    cout << sReturnAverageAgeOfPeople(people) << endl;
+
+    // struct returning tests - Person
+    Person randomPerson = makeRandomPerson();
+    cout << "Random Person: ";
+    displayPerson(randomPerson);
+
+    Person personAge = makePersonAgeYearsOld(30);
+    cout << "Person Age 30: ";
+    displayPerson(personAge);
+
+    Person personHalfAge = makePersonAgeAndAHalfYearsOld(25.5);
+    cout << "Person Age 25.5: ";
+    displayPerson(personHalfAge);
+
+    Person namedPerson = makePersonWithName("Idil Kolabas");
+    cout << "Named Person: ";
+    displayPerson(namedPerson);
+
+    Person copyPerson = makeCopyOfPerson(namedPerson);
+    cout << "Copy of Person: ";
+    displayPerson(copyPerson);
+
+    Person olderRelative = returnRelativeAYearOlder(idils_sister);
+    cout << "Relative a year older: ";
+    displayPerson(olderRelative);
+
+    Person oldestMember = returnOldestMember(fot);
+    cout << "Oldest member of family: ";
+    displayPerson(oldestMember);
+
+    Person youngestMember = returnYoungestMember(persons);
+    cout << "Youngest member of persons: ";
+    displayPerson(youngestMember);
+
+    Person oldestAvgFamily = returnOldestAverageFamily(families);
+    cout << "Oldest average family member: ";
+    displayPerson(oldestAvgFamily);
+
+    Person youngestAvgPeople = returnYoungestAveragePeople(people);
+    cout << "Youngest average people: ";
+    displayPerson(youngestAvgPeople);
+
+    // struct returning tests - People or FoT
+    FamilyOfThree randomFamily = makeFamilyOfThree();
+    cout << "Random Family of Three:" << endl;
+    displayFamily(randomFamily);
+
+    FamilyOfThree ageFamily = makeFamilyWithAges(25, 35, 45);
+    cout << "Family with Ages 25, 35, 45:" << endl;
+    displayFamily(ageFamily);
+
+    FamilyOfThree namedFamily = makeFamilyWithName("Kolabas");
+    cout << "Family with Lastname Kolabas:" << endl;
+    displayFamily(namedFamily);
+
+    Person p1 = {"Alice", 30};
+    Person p2 = {"Bob", 28};
+    Person p3 = {"Charlie", 5};
+    FamilyOfThree personFamily = makeFamilyWithPersons(p1, p2, p3);
+    cout << "Family with Specific Persons:" << endl;
+    displayFamily(personFamily);
+
+    Person fofthree[3] = {{"Dave", 45}, {"Eve", 40}, {"Frank", 10}};
+    FamilyOfThree arrayFamily = makeFamilyWithPersonArray(fofthree);
+    cout << "Family with Persons Array:" << endl;
+    displayFamily(arrayFamily);
+
+    FamilyOfThree five_families[5] = {
+        createFamily("Smith", {"John", 40}, {"Jane", 38}, {"Jack", 15}),
+        createFamily("Doe", {"Joe", 42}, {"Jill", 39}, {"Jenny", 16}),
+        createFamily("Brown", {"Jim", 45}, {"Joan", 43}, {"Josh", 18}),
+        createFamily("Taylor", {"Tim", 50}, {"Tina", 48}, {"Tom", 20}),
+        createFamily("White", {"Will", 55}, {"Wendy", 52}, {"Walter", 25})
+    };
+    FamilyOfThree oldestFamily = returnOldestFamily(five_families);
+    cout << "Oldest Family based on Age Sum:" << endl;
+    displayFamily(oldestFamily);
 
     return 0;
 }
